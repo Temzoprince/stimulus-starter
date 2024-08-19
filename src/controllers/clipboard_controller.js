e83906a73c2454cfb05b079d103dd2950209e988
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-08-19 12:21:08 +0100

    Button only works with api support

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -3,7 +3,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static classes = [ 'supported' ]
     static targets = [ 'source' ]
+
+    connect() {
+        if ( 'clipboard' in navigator) {
+            this.element.classList.add(this.supportedClass)
+        }
+    }
 
     copy(event) {
         event.preventDefault()
